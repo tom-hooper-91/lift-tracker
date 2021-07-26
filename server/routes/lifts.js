cf524a179d682ec8c97e9ee2443926798ca57567
Tom Hooper <tomwbhooper@hotmail.com>
2021-07-27 11:45:25 +1200

    write first get route and test

--- server/routes/lifts.js
+++ server/routes/lifts.js
@@ -1,18 +1,18 @@
 const express = require('express')
 
-const db = require('../db/fruits')
+const db = require('../db/connection')
 
 const router = express.Router()
 
 router.get('/', (req, res) => {
-  db.getFruits()
+  db.getAllLifts()
     .then(results => {
-      res.json({ fruits: results.map(fruit => fruit.name) })
+      res.json(results)
       return null
     })
     .catch(err => {
       console.log(err)
-      res.status(500).json({ message: 'Somthing went wrong' })
+      res.status(500).json({ message: 'Something went wrong ' })
     })
 })
 
