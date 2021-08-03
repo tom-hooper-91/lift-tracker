b9f9dd4bbd1b4979d95d277ca6654ea400b76f7e
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-03 17:10:29 +1200

    add local state to manage AddExercise form

--- client/components/AddExercise.jsx
+++ client/components/AddExercise.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import { capitalize } from '../utils'
 
 const AddExercise = ({ category }) => {
+  const [formData, setFormData] = useState({
+    exercise: '',
+    category: ''
+  })
+
   const handleSubmit = (e) => {
     e.preventDefault()
+  }
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value })
   }
   return (
     <>
@@ -14,11 +23,11 @@ const AddExercise = ({ category }) => {
           <form action="submit" className='row gy-2 gx-3 align-items-center' onSubmit={(e) => handleSubmit(e)}>
             <div className="col-auto">
               <label htmlFor="autoSizingInput">Exercise</label>
-              <input name='exercise' type="text" className='form-control' id='autoSizingInput' placeholder='Exercise'/>
+              <input name='exercise' type="text" className='form-control' id='autoSizingInput' placeholder='Exercise' value={formData.exercise} onChange={(e) => handleChange(e)}/>
             </div>
             <div className="col-auto">
               <label htmlFor="autoSizingSelect">Category</label>
-              <select name="category" className='form-select' id="autoSizingSelect">
+              <select name="category" className='form-select' id="autoSizingSelect" value={formData.category} onChange={(e) => handleChange(e)}>
                 <option>Choose...</option>
                 <option value={category}>{capitalize(category)}</option>
                 {category === 'chest' ? <option value='biceps'>Biceps</option> : null}
