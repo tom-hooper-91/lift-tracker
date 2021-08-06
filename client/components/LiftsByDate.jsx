e4a5b85c64bd4b218028278921498b12070245e1
Tom Hooper <tomwbhooper@hotmail.com>
2021-08-06 14:29:08 +1200

    add table of lifts by date to LiftsByDate

--- client/components/LiftsByDate.jsx
+++ client/components/LiftsByDate.jsx
@@ -1,11 +1,48 @@
 import React from 'react'
+import { connect } from 'react-redux'
 
-const LiftsByDate = ({ date }) => {
+import { displayExercise } from '../utils'
+
+const LiftsByDate = ({ date, lifts }) => {
   return (
     <>
-      <h1>{date}</h1>
+      <div className="row">
+        <div className="col-12">
+          <h1>All lifts on {date}</h1>
+          <table className='table table-striped'>
+            <thead>
+              <tr>
+                <th scope='col'>Exercise</th>
+                <th scope='col'>Weight</th>
+                <th scope='col'>Reps</th>
+                <th scope='col'>Date</th>
+              </tr>
+            </thead>
+            <tbody>
+              {lifts.filter(l => l.date === date).map(l => {
+                return (
+                  l.weight && l.reps
+                    ? <tr key={l.id}>
+                      <th scope='row'>{displayExercise(l.exercise)}</th>
+                      <td>{l.weight}</td>
+                      <td>{l.reps}</td>
+                      <td>{l.date}</td>
+                    </tr>
+                    : null
+                )
+              })}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
   )
 }
 
-export default LiftsByDate
+const mapStateToProps = (globalState) => {
+  return {
+    lifts: globalState.lifts
+  }
+}
+
+export default connect(mapStateToProps)(LiftsByDate)
