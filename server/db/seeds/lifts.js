b4e71e5dd32d7879468dd6a044bb9b0e24a2df6d
Tom Hooper <tomwbhooper@hotmail.com>
2021-09-01 10:25:41 +1200

    add 20 new entries for seed file

--- server/db/seeds/lifts.js
+++ server/db/seeds/lifts.js
@@ -13,7 +13,27 @@ exports.seed = function (knex) {
         { id: 7, category: 'back', exercise: 'deadlift', weight: 60, reps: 5, date: '2021-08-01' },
         { id: 8, category: 'shoulders', exercise: 'overhead_press', weight: 40, reps: 5, date: '2021-08-01' },
         { id: 9, category: 'legs', exercise: 'squat', weight: 50, reps: 5, date: '2021-08-01' },
-        { id: 10, category: 'biceps', exercise: 'ez_curl', weight: 10, reps: 8, date: '2021-08-01' }
+        { id: 10, category: 'biceps', exercise: 'ez_curl', weight: 10, reps: 8, date: '2021-08-01' },
+        { id: 11, category: 'chest', exercise: 'bench_press', weight: 50, reps: 5, date: '2021-08-01' },
+        { id: 12, category: 'back', exercise: 'deadlift', weight: 60, reps: 5, date: '2021-08-01' },
+        { id: 13, category: 'shoulders', exercise: 'overhead_press', weight: 40, reps: 5, date: '2021-08-01' },
+        { id: 14, category: 'legs', exercise: 'squat', weight: 50, reps: 5, date: '2021-08-01' },
+        { id: 15, category: 'biceps', exercise: 'ez_curl', weight: 10, reps: 8, date: '2021-08-01' },
+        { id: 16, category: 'chest', exercise: 'bench_press', weight: 50, reps: 5, date: '2021-08-01' },
+        { id: 17, category: 'back', exercise: 'deadlift', weight: 60, reps: 5, date: '2021-08-01' },
+        { id: 18, category: 'shoulders', exercise: 'overhead_press', weight: 40, reps: 5, date: '2021-08-01' },
+        { id: 19, category: 'legs', exercise: 'squat', weight: 50, reps: 5, date: '2021-08-01' },
+        { id: 20, category: 'biceps', exercise: 'ez_curl', weight: 10, reps: 8, date: '2021-08-01' },
+        { id: 21, category: 'chest', exercise: 'bench_press', weight: 50, reps: 5, date: '2021-08-01' },
+        { id: 22, category: 'back', exercise: 'deadlift', weight: 60, reps: 5, date: '2021-08-01' },
+        { id: 23, category: 'shoulders', exercise: 'overhead_press', weight: 40, reps: 5, date: '2021-08-01' },
+        { id: 24, category: 'legs', exercise: 'squat', weight: 50, reps: 5, date: '2021-08-01' },
+        { id: 25, category: 'biceps', exercise: 'ez_curl', weight: 10, reps: 8, date: '2021-08-01' },
+        { id: 26, category: 'chest', exercise: 'bench_press', weight: 50, reps: 5, date: '2021-08-01' },
+        { id: 27, category: 'back', exercise: 'deadlift', weight: 60, reps: 5, date: '2021-08-01' },
+        { id: 28, category: 'shoulders', exercise: 'overhead_press', weight: 40, reps: 5, date: '2021-08-01' },
+        { id: 29, category: 'legs', exercise: 'squat', weight: 50, reps: 5, date: '2021-08-01' },
+        { id: 30, category: 'biceps', exercise: 'ez_curl', weight: 10, reps: 8, date: '2021-08-01' }
       ])
     })
 }
